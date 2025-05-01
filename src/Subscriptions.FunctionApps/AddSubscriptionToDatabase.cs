7fd935f2f9f373f0912cbd30b799964d43ec149f
andyvernon <andy_vernon@live.co.uk>
2025-05-01 21:59:29 +0100

    Created first function from scratch

---
+++ src/Subscriptions.FunctionApps/AddSubscriptionToDatabase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Subscriptions.FunctionApps.Models;
+using System.Text.Json;
+
+namespace Subscriptions.FunctionApps
+{
+    public class AddSubscriptionToDatabase
+    {
+        private readonly ILogger _logger;
+
+        public AddSubscriptionToDatabase(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AddSubscriptionToDatabase>();
+        }
+
+        [Function("AddSubscriptionToDatabase")]
+        [CosmosDBOutput(
+            databaseName: "SubscriptionsDB",
+            containerName: "Subscriptions",
+            Connection = "CosmosDbConnectionString")]
+        public Subscription? Run(
+            [QueueTrigger(
+            "customer-subscriptions", 
+            Connection = "AzureWebJobsStorage")] string queueItem)
+        {
+            _logger.LogInformation($"Received queue message: {queueItem}");
+
+            var subscription = JsonSerializer.Deserialize<Subscription>(queueItem);
+
+            if (subscription == null)
+            {
+                _logger.LogError("Failed to deserialize queue item into Subscription object.");
+                return null;
+            }
+
+            return subscription;
+        }
+    }
+}
---
+++ src/Subscriptions.FunctionApps/Models/Subscription.cs
@@ -0,0 +1,8 @@
+﻿namespace Subscriptions.FunctionApps.Models
+{
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
